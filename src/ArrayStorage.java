f7453f2c2bb46efe3e9bcaa0ea7a6444a7bfdcc4
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-01-01 20:04:01 +0300

    add Resume get

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,18 +1,37 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int cell;
 
     void clear() {
+        Arrays.fill(storage, null);
+        cell = 0;
     }
 
     void save(Resume r) {
+        storage[cell++] = r;
     }
 
     //проверить совпадение имени на входе и в массиве при совпадении выдать имя, если нет вывести: Нет "uuid" в массиве
     Resume get(String uuid) {
-        return null;
+        int i;
+        for (i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                System.out.println("Совпадений с " + uuid + " в массиве не обнаружено");
+                break;
+            }
+            if (storage[i].toString() == uuid) {
+                break;
+            }
+            /*System.out.println("Моё uuid = " + uuid);
+            System.out.println("Моё resume = " + storage[i]);
+            System.out.println("Моё " + storage[i].toString() == uuid);*/
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
@@ -26,6 +45,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        return 0;
+        return cell;
     }
 }
