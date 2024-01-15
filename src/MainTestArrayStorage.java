8dd9765a799c0b525fe825305d3141f9b9e40c41
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-01-15 21:04:01 +0300

    udateMainTestArrayStorage 26_27

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -21,6 +21,10 @@ public class MainTestArrayStorage {
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
+
+
+        printAll();
+        ARRAY_STORAGE.update(r1);
         printAll();
         ARRAY_STORAGE.delete(r1.uuid);
         printAll();
