c80d6b9216372613fdef732b5f4e26719c9d5212
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-02-03 22:55:39 +0300

    update binary sort in save()

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -19,7 +19,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
                 cell = cell * -1 -1;
                 Resume temp = storage[countResumes - 1];
                 for (int i = countResumes; i > cell; i--) {
-                    storage[i] = storage[i-1];
+                    storage[i] = storage[i - 1];
                 }
                 storage[cell] = temp;
             }
@@ -30,45 +30,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-//        System.out.println(uuid + " binarySearch = " + Arrays.binarySearch(storage, 0, countResumes, searchKey));
         return Arrays.binarySearch(storage, 0, countResumes, searchKey);
     }
 }
-
-/*
-* import java.util.Arrays;
-
-class GFG
-{
-
-    public static void main(String[] args)
-    {
-        final String[] arr = {"uuid1", "uuid4", "uuid3", "uuid5", "uuid7", "uuid6", "uuid2"};
-
-        new GFG().sort(arr);
-
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i] + " ");
-    }
-
-    // Driver Code
-    public void sort(String  array[])
-    {
-        for (int i = 1; i < array.length; i++)
-        {
-            String x = array[i];
-
-            // Find location to insert
-            // using binary search
-            int j = Math.abs(Arrays.binarySearch(array, 0, i, x) + 1);
-
-            // Shifting array to one
-            // location right
-            System.arraycopy(array, j, array, j + 1, i - j);
-
-            // Placing element at its
-            // correct location
-            array[j] = x;
-        }
-    }
-}*/
