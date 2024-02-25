dc6f36d653924c31a7952ce53030307243f72ae5
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-02-25 14:18:06 +0300

    Lesson04 add exceptions

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -53,7 +53,7 @@ public abstract class AbstractArrayStorage implements Storage {
     }
 
     @Override
-    final public void delete (String uuid) throws NotExistStorageException {
+    final public void delete (String uuid) {
         int index = getIndex(uuid);
         if (index < 0) {
             throw new NotExistStorageException(uuid);
