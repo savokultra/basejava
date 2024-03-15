32f34bbb2416a2b7c3fc23bd031d077e65cca036
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-03-15 18:06:13 +0300

    Reflections annotations tests

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,10 +1,25 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
 
-public class AbstractArrayStorageTest {
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
 
     @Test
     public void clear() {
@@ -22,6 +37,11 @@ public class AbstractArrayStorageTest {
     public void get() {
     }
 
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
+
     @Test
     public void delete() {
     }
@@ -32,5 +52,6 @@ public class AbstractArrayStorageTest {
 
     @Test
     public void size() {
+        Assert.assertEquals(3, storage.size());
     }
 }
