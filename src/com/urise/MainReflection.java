2f66f3f624943c96ee03ce07f9724ba368463fbf
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-04-06 22:27:53 +0300

    TODO : invoke r.toString via reflection*/

--- src/com/urise/MainReflection.java
+++ src/com/urise/MainReflection.java
@@ -1,10 +1,13 @@
 package com.urise;
 
 import com.urise.webapp.model.Resume;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -12,6 +15,9 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection*/
+//        https://stackoverflow.com/questions/160970/how-do-i-invoke-a-java-method-when-given-the-method-name-as-a-string
+        Method method = r.getClass().getMethod("toString");
+        System.out.println(method.invoke(r));
         System.out.println(r);
     }
 }
