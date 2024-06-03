9fd221d6d2dbcd1eb55bee82666b1080b0d5052c
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-06-04 00:27:06 +0300

    add list storage and try some methods

--- src/com/urise/webapp/storage/ListStorage.java
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -7,10 +7,15 @@ import java.util.ArrayList;
 public class ListStorage extends AbstractStorage {
     static ArrayList<Resume> list = new ArrayList<>();
 
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
     public static void main(String[] args) {
         list.add(new Resume("uuid_1"));
         System.out.println(list.get(0));
-//        list.clear();
+        list.clear();
         String string = list.get(0).toString();
         System.out.println(string);
     }
