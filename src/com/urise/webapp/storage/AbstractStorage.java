29a4c1bd1cdb020c12fd98caf8584eadba533d88
Slava_Ilyin <savok.ultrasound@gmail.com>
2024-06-03 23:17:28 +0300

    add abstract storage

---
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -0,0 +1,41 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public class AbstractStorage implements Storage {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
