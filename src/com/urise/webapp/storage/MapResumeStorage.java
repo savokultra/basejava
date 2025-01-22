1a84c7a8de96d102cc6041632053efbe901302e1
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-01-22 12:42:46 +0300

    add MapResumeStorage

---
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -0,0 +1,57 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapResumeStorage extends AbstractStorage {
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return map.get(uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Object searchKey) {
+        map.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return searchKey != null;
+    }
+
+    @Override
+    protected void doSave(Resume resume, Object searchKey) {
+        map.put(resume.getUuid(), resume);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return (Resume) searchKey;
+    }
+
+    @Override
+    public void doDelete(Object searchKey) {
+        map.remove((Resume) searchKey).getUuid();
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        return new ArrayList<>(map.values());
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+}
