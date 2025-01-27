2f7331396714d0261822e0aeaaceea90558d3a1c
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-01-27 12:14:59 +0300

    add generic in MapResumeStorage

--- src/com/urise/webapp/storage/MapResumeStorage.java
+++ src/com/urise/webapp/storage/MapResumeStorage.java
@@ -7,37 +7,37 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MapResumeStorage extends AbstractStorage {
+public class MapResumeStorage extends AbstractStorage<Resume> {
     private final Map<String, Resume> map = new HashMap<>();
 
     @Override
-    protected Object getSearchKey(String uuid) {
+    protected Resume getSearchKey(String uuid) {
         return map.get(uuid);
     }
 
     @Override
-    protected void doUpdate(Resume resume, Object searchKey) {
+    protected void doUpdate(Resume resume, Resume searchKey) {
         map.put(resume.getUuid(), resume);
     }
 
     @Override
-    protected boolean isExist(Object searchKey) {
+    protected boolean isExist(Resume searchKey) {
         return searchKey != null;
     }
 
     @Override
-    protected void doSave(Resume resume, Object searchKey) {
+    protected void doSave(Resume resume, Resume searchKey) {
         map.put(resume.getUuid(), resume);
     }
 
     @Override
-    protected Resume doGet(Object searchKey) {
-        return (Resume) searchKey;
+    protected Resume doGet(Resume searchKey) {
+        return searchKey;
     }
 
     @Override
-    public void doDelete(Object searchKey) {
-        map.remove(((Resume) searchKey).getUuid());
+    public void doDelete(Resume searchKey) {
+        map.remove(searchKey.getUuid());
     }
 
     @Override
