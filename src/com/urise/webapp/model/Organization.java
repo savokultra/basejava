082b9f43230ec0cbe245177e6000f7bc46ba3633
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-02-13 16:20:59 +0300

    start make Organization class

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,4 +1,24 @@
 package com.urise.webapp.model;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Organization {
+    private final Link homePage;
+
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final String title;
+    private final String description;
+
+    public Organization(String name, String url, LocalDate startDate, LocalDate endDate, String title, String description) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(endDate, "endDate must not be null");
+        Objects.requireNonNull(title, "title must not be null");
+        this.homePage = new Link(name, url);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.description = description;
+    }
 }
