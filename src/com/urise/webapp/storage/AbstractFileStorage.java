1125c84e914910ef47cdf46cb50f4075b2add38f
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-02-24 22:00:13 +0300

    add StorageException AbstractFileStorage

--- src/com/urise/webapp/storage/AbstractFileStorage.java
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -1,4 +1,76 @@
 package com.urise.webapp.storage;
 
-public class AbstractFileStorage {
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private File directory;
+
+    public AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + " is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, File searchKey) {
+
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected void doSave(Resume resume, File file) {
+        try {
+            file.createNewFile();
+            doWrite(resume, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    protected abstract void doWrite(Resume resume, File file) throws IOException;
+
+    @Override
+    protected Resume doGet(File file) {
+        return null;
+    }
+
+    @Override
+    public void doDelete(File file) {
+
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        return null;
+    }
 }
