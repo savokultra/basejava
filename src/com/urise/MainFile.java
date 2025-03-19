1d6ec5ef073672df00b1a75174aeeb5c4940a9c2
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-03-19 12:23:12 +0300

    update main file, add offset

--- src/com/urise/MainFile.java
+++ src/com/urise/MainFile.java
@@ -29,21 +29,21 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        printDirectoryDeeply(dir);
-        }
+        printDirectoryDeeply(dir, "");
+    }
 
-        public static void printDirectoryDeeply (File dir){
-            File[] files = dir.listFiles();
+    public static void printDirectoryDeeply(File dir, String offset) {
+        File[] files = dir.listFiles();
 
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile()) {
-                        System.out.println("File: " + file.getName());
-                    } else if (file.isDirectory()) {
-                        System.out.println("Directory: " + file.getName());
-                        printDirectoryDeeply(file);
-                    }
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(offset + "F: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println(offset + "D: " + file.getName());
+                    printDirectoryDeeply(file, offset + "  ");
                 }
             }
         }
     }
+}
