02daae2f35499e77b5e407579a57040194549c06
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-03-24 22:09:51 +0300

    update pathStorage and all tests complited

--- src/com/urise/webapp/storage/PathStorage.java
+++ src/com/urise/webapp/storage/PathStorage.java
@@ -11,6 +11,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PathStorage extends AbstractStorage<Path> {
     private Path directory;
@@ -19,6 +21,7 @@ public class PathStorage extends AbstractStorage<Path> {
 
     protected PathStorage(String dir, StreamSerializer streamSerializer) {
         Objects.requireNonNull(dir, "Directory must not be null");
+
         this.streamSerializer = streamSerializer;
         directory = Paths.get(dir);
         if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
@@ -41,37 +44,62 @@ public class PathStorage extends AbstractStorage<Path> {
     }
 
     @Override
-    protected boolean isExist(Path searchKey) {
-        return false;
+    protected boolean isExist(Path path) {
+        return Files.isRegularFile(path);
     }
 
     @Override
-    protected void doSave(Resume resume, Path searchKey) {
-
+    protected void doSave(Resume resume, Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create path " + path, getFilename(path), e);
+        }
+        doUpdate(resume, path);
     }
 
     @Override
-    protected Resume doGet(Path searchKey) {
-        return null;
+    protected Resume doGet(Path path) {
+        try {
+            return streamSerializer.doRead(new BufferedInputStream(Files.newInputStream(path)));
+        } catch (IOException e) {
+            throw new StorageException("Path read error", getFilename(path), e);
+        }
     }
 
     @Override
-    public void doDelete(Path searchKey) {
-
+    public void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", getFilename(path), e);
+        }
     }
 
     @Override
     protected List<Resume> doCopyAll() {
-        return List.of();
+        return getFilesList().map(this::doGet).collect(Collectors.toList());
     }
 
     @Override
     public void clear() {
-
+        getFilesList().forEach(this::doDelete);
     }
 
     @Override
     public int size() {
-        return 0;
+        return (int) getFilesList().count();
+    }
+
+    private String getFilename(Path path) {
+        return path.getFileName().toString();
+    }
+
+    private Stream<Path> getFilesList() {
+        try {
+            return Files.list(directory);
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", e);
+        }
     }
 }
