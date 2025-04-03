78693dd0ead3f302660ce14933495fd4db785249
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-04-03 12:40:04 +0300

    add XmlClasses

---
+++ src/com/urise/webapp/storage/serializer/XmlStreamSerializer.java
@@ -0,0 +1,30 @@
+package com.urise.webapp.storage.serializer;
+
+import com.urise.webapp.model.*;
+import com.urise.webapp.util.XmlParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class XmlStreamSerializer implements StreamSerializer {
+    private XmlParser xmlParser;
+
+    public XmlStreamSerializer() {
+        xmlParser = new XmlParser(Resume.class, Organization.class, Link.class,
+                OrganizationSection.class, TextSection.class, ListSection.class, Organization.Position.class);
+    }
+
+    @Override
+    public void doWrite(Resume resume, OutputStream outputStream) throws IOException {
+        try (Writer writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)) {
+            xmlParser.marshall(resume, writer);
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream inputStream) throws IOException {
+        try (Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+            return xmlParser.unmarshall(reader);
+        }
+    }
+}
