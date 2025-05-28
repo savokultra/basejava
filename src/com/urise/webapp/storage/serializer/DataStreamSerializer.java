b3fc277f2575b1ebfeae8c396c4d65cc2e813fb0
Slava_Ilyin <savok.ultrasound@gmail.com>
2025-05-28 12:58:01 +0300

    hw_10_3 add DataStreamSerializer

---
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -0,0 +1,39 @@
+package com.urise.webapp.storage.serializer;
+
+import com.urise.webapp.model.ContactType;
+import com.urise.webapp.model.Resume;
+
+import java.io.*;
+import java.util.Map;
+
+public class DataStreamSerializer implements StreamSerializer {
+    @Override
+    public void doWrite(Resume resume, OutputStream outputStream) throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(outputStream)) {
+            dos.writeUTF(resume.getUuid());
+            dos.writeUTF(resume.getFullName());
+            Map<ContactType, String> contacts = resume.getContacts();
+            dos.writeInt(contacts.size());
+            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
+            }
+            // TODO implevents sections
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream inputStream) throws IOException {
+        try (DataInputStream dis = new DataInputStream(inputStream)) {
+            String uuid = dis.readUTF();
+            String fullName = dis.readUTF();
+            Resume resume = new Resume(uuid, fullName);
+            int size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+            }
+            //TODO implements stctions
+            return null;
+        }
+    }
+}
