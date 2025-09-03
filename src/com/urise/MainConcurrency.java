6f3023b46078ca7fa780b33e7521eefb79db2e07
Slava <savok.ultrasound@gmail.com>
2025-09-03 12:55:13 +0300

    add ReentrantLock in MainConcurrency

--- src/com/urise/MainConcurrency.java
+++ src/com/urise/MainConcurrency.java
@@ -1,14 +1,14 @@
 package com.urise;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
-    private static final Object LOCK = new Object();
+//    private static final Object LOCK = new Object();
+    private static final Lock lock = new ReentrantLock();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -41,19 +41,22 @@ public class MainConcurrency {
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
         CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
-        ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
 
 //        List<Thread> treads = new ArrayList<>(THREADS_NUMBER);
 
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            executorService.submit(() ->
+            Future<Integer> future = executorService.submit(() ->
 //            Thread thread = new Thread(() ->
             {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
                 }
                 latch.countDown();
+                return 5;
             });
+
 //            thread.start();
 //            treads.add(thread);
         }
@@ -98,10 +101,15 @@ public class MainConcurrency {
     }
 
 
-    private synchronized void inc() {
+    private void inc() {
 //        synchronized (this) {
 //        synchronized (MainConcurrency.class) {
-        counter++;
+        lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }
 //                wait();
 //                readFile
 //                ...
